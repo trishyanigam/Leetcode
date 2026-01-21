5af43c6164b358948530f2efcd8e9cb14c76722b
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-01-21 21:03:17 +0530

    Time: 4 ms (24.07%), Space: 23.9 MB (9.3%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word,res;
+        while(ss>>word)
+        {
+            res=word+" "+res;
+        }
+        if(!res.empty())
+        {
+            res.pop_back();
+        }
+        return res;
+    }
+};
