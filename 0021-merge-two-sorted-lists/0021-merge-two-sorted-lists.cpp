493d655a2f5fe48fc8cccf819626b4664e7baab3
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-01-23 16:17:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -18,24 +18,24 @@ public:
             if(list1->val<=list2->val)
             {
                 dummy->next = list1;
-                list1 = list1->next;
+                list1=list1->next;
             }
             else
             {
                 dummy->next = list2;
-                list2 = list2->next;
+                list2=list2->next;
             }
-            dummy=dummy->next;
+            dummy = dummy->next;
         }
         while(list1!=NULL)
         {
-            dummy->next = list1;
+            dummy->next=list1;
             list1=list1->next;
             dummy=dummy->next;
         }
         while(list2!=NULL)
         {
-            dummy->next = list2;
+            dummy->next=list2;
             list2=list2->next;
             dummy=dummy->next;
         }
