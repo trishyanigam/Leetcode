edd46bb2120953e40f640e6db8a0e15bb91dc2ea
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-01-26 13:44:41 +0530

    Time: 3 ms (42.53%), Space: 12.8 MB (58.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size();
         int n2=nums2.size();
-        stack<int>st;
+        stack<int> st;
         unordered_map<int,int>mp;
+        vector<int>ans;
         for(int i=n2-1;i>=0;i--)
         {
             while(!st.empty() && st.top()<=nums2[i])
@@ -21,7 +22,6 @@ public:
             }
             st.push(nums2[i]);
         }
-        vector<int> ans;
         for(int i=0;i<n1;i++)
         {
             ans.push_back(mp[nums1[i]]);
