c9fd7dbd3d4309574100f27a6e167fc13262d645
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-01-29 15:47:51 +0530

    Time: 3 ms (28.88%), Space: 17.1 MB (32.39%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,8 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -16,9 +16,9 @@ public:
                 }
             }
         }
+        int time = 0;
         int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
-        int time = 0;
         while(!q.empty())
         {
             int r = q.front().first.first;
@@ -26,15 +26,14 @@ public:
             int t = q.front().second;
             q.pop();
             time = max(time,t);
-
             for(int i=0;i<4;i++)
             {
                 int nrow = r+drow[i];
                 int ncol = c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1)
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0)
                 {
                     q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=1;
+                    vis[nrow][ncol] = 1;
                 }
             }
         }
