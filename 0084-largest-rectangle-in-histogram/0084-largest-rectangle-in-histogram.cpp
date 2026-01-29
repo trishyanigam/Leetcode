4701d534a1fd17985f64718f6b33c6d088b17a5f
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-01-29 20:55:45 +0530

    Time: 16 ms (81%), Space: 85.8 MB (24.68%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,28 +1,57 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int maxi=0;
-        stack<int> st;
-        int n=heights.size();
-        for(int i=0;i<=n;i++)
+        int n = heights.size();
+        vector<int>pse(n,0);
+        vector<int>nse(n,0);
+        stack<int>st;
+        for(int i=0;i<n;i++)
         {
-            while(!st.empty()&& (i==n || heights[st.top()]>=heights[i]))
+            while(!st.empty() && heights[st.top()]>=heights[i])
             {
-                int height=heights[st.top()];
                 st.pop();
-                int width;
-                if(st.empty())
-                {
-                    width=i;
-                }
-                else
-                {
-                    width=i-st.top()-1;
-                }
-                maxi=max(maxi,width*height);
             }
-             st.push(i);
+            if(st.empty())
+            {
+                pse[i]=-1;
+            }
+            else
+            {
+                pse[i]=st.top();
+            }
+            st.push(i);
         }
-        return maxi;
+
+        while(!st.empty())
+        {
+            st.pop();
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && heights[st.top()]>=heights[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+            {
+                nse[i]=n;
+            }
+            else
+            {
+                nse[i]=st.top();
+            }
+            st.push(i);
+        }
+        
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            int h = heights[i];
+            int w = nse[i]-pse[i]-1;
+            int ar = h*w;
+            ans = max(ans,ar);
+        }
+        return ans;
     }
 };
