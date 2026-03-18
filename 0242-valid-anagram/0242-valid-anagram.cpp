9b5a6db53a688b4a89f21a8baf74d9f2e3bb2075
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-18 18:15:23 +0530

    Time: 0 ms (100%), Space: 9.6 MB (73.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-        {
-            return true;
-        }
+        if(s.size()!=t.size())
         return false;
+
+        vector<int>freq(26,0);
+        for(char c:s)
+        {
+            freq[c-'a']++;
+        }
+        for(char c:t)
+        {
+            freq[c-'a']--;
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
