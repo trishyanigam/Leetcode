8feef0fcbc90a30702d5c11b17bfb8af2e41fa55
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-18 18:02:30 +0530

    Time: 63 ms (19.98%), Space: 92.9 MB (16.64%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int x:nums)
+        {
+            mp[x]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second>1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
