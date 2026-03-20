fd4be804a01c00a928e05b36b66342731ee2f557
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-20 10:54:48 +0530

    Time: 4 ms (41.11%), Space: 23.8 MB (42.63%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<set<char>>rows(9),cols(9),boxes(9);
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char val = board[i][j];
+                if(val=='.')
+                {
+                    continue;
+                }
+                int idx=(i/3)*3+(j/3);
+                if(rows[i].count(val) || cols[j].count(val) || boxes[idx].count(val))
+                {
+                    return false;
+                }
+                rows[i].insert(val);
+                cols[j].insert(val);
+                boxes[idx].insert(val);
+            }
+        }
+        return true;
+    }
+};
