d15db0da48a15aa345286be9200fab10a0d8f2ff
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-21 11:09:14 +0530

    Time: 55 ms (29.57%), Space: 29 MB (71.94%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int n = nums.size();
+        int n=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
@@ -10,42 +10,42 @@ public:
             {
                 continue;
             }
-            int j = i+1;
-            int k = n-1;
+            int j=i+1;
+            int k=n-1;
             while(j<k)
             {
-                int sum = nums[i]+nums[j]+nums[k];
-            if(sum==0)
-            {
-                ans.push_back({nums[i],nums[j],nums[k]});
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1])
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
                 {
+                    ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
+                    k--;
+                    while(j<k && nums[k]==nums[k+1])
+                    {
+                        k--;
+                    }
+                    while(j<k && nums[j]==nums[j-1])
+                    {
+                        j++;
+                    }
                 }
-                while(j<k && nums[k]==nums[k+1])
+                else if(sum>0)
                 {
                     k--;
+                    while(j<k && nums[k]==nums[k+1])
+                    {
+                        k--;
+                    }
                 }
-            }
-            else if(sum<0)
-            {
-                j++;
-                while(j<k && nums[j]==nums[j-1])
+                else
                 {
                     j++;
+                    while(j<k && nums[j]==nums[j-1])
+                    {
+                        j++;
+                    }
                 }
             }
-            else
-            {
-                k--;
-                while(j<k && nums[k]==nums[k+1])
-                {
-                    k--;
-                }
-            }
-            }       
         }
         return ans;
     }
