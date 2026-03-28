fc5fc7b788f48d162f1aec661bbca6ea600ae43b
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-28 11:57:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maximum(vector<int>& piles)
+    {
+        int maxi=INT_MIN;
+        for(int i=0;i<piles.size();i++)
+        {
+            maxi=max(maxi,piles[i]);
+        }
+        return maxi;
+    }
+    int calculate(vector<int>& piles,int val)
+    {
+        int ans=0;
+        for(int i=0;i<piles.size();i++)
+        {
+            ans+=ceil((double)piles[i]/(double)val);
+        }
+        return ans;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1;
+        int high=maximum(piles);
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            int tot = calculate(piles,mid);
+            if(tot<=h)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
