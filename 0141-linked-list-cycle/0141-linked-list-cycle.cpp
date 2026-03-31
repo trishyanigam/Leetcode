5bb41424b02c02ef0b1dc9bd2f832a80c4e6d335
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-03-31 12:50:56 +0530

    Time: 8 ms (83.15%), Space: 11.9 MB (22.14%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -13,9 +13,9 @@ public:
         {
             return false;
         }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast!=NULL && fast->next!=NULL)
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
