b90f9f071a3795ecc41d4e87525634e0bb3ee84f
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-16 10:37:13 +0530

    Time: 7 ms (35.93%), Space: 27.1 MB (82.2%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,9 +11,9 @@
  */
 class Solution {
 public:
-    int search(vector<int>& inorder,int left,int right,int val)
+    int search(int l,int r, vector<int>& inorder,int val)
     {
-        for(int i=left;i<=right;i++)
+        for(int i=l;i<=r;i++)
         {
             if(inorder[i]==val)
             {
@@ -22,21 +22,21 @@ public:
         }
         return -1;
     }
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder,int& idx,int left,int right)
+    TreeNode* helper(vector<int>& preorder, vector<int>& inorder,int &idx,int l,int r)
     {
-        if(left>right)
+        if(l>r)
         {
             return NULL;
         }
         TreeNode* root = new TreeNode(preorder[idx]);
-        int inIdx = search(inorder,left,right,preorder[idx]);
+        int inidx = search(l,r,inorder,preorder[idx]);
         idx++;
-        root->left = helper(preorder,inorder,idx,left,inIdx-1);
-        root->right = helper(preorder,inorder,idx,inIdx+1,right);
+        root->left = helper(preorder,inorder,idx,l,inidx-1);
+        root->right = helper(preorder,inorder,idx,inidx+1,r);
         return root;
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int idx=0;
-        return helper(preorder,inorder,idx,0,inorder.size()-1);
+        return helper(preorder,inorder,idx,0,preorder.size()-1);
     }
 };
