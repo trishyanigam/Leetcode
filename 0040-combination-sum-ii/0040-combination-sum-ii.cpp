7dff76c30987d40049f666b9dc5faaf672e78f21
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-18 14:17:04 +0530

    Time: 0 ms (100%), Space: 13.9 MB (81.63%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    void helper(int st,vector<int>& arr,int tar,vector<int>& curr,vector<vector<int>>& ans)
+    void helper(int i,vector<int>& arr,int tar,vector<int>& path,vector<vector<int>>& ans)
     {
-        if(tar==0)
-        {
-            ans.push_back(curr);
-            return;
-        }
-        for(int i=st;i<arr.size();i++)
-        {
-            if(i>st && arr[i]==arr[i-1])
-            continue;
+        int n=arr.size();
+            if(tar==0)
+            {
+                ans.push_back(path);
+                return;
+            }
 
-            if(arr[i]>tar)
-            break;
-
-            curr.push_back(arr[i]);
-            helper(i+1,arr,tar-arr[i],curr,ans);
-            curr.pop_back();
+        for(int j=i;j<n;j++)
+        {
+            if(j>i && arr[j]==arr[j-1])
+            {
+                continue;
+            }
+            if(arr[j]>tar)
+            {
+                break;
+            }
+            path.push_back(arr[j]);
+            helper(j+1,arr,tar-arr[j],path,ans);
+            path.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
-        vector<int> curr;
-        helper(0,candidates,target,curr,ans);
+        vector<int> path;
+        helper(0,candidates,target,path,ans);
         return ans;
     }
 };
