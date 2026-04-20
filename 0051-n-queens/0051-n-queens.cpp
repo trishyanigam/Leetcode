d6057bc310b214e33ebcd9d2a2f9b57c5ace7462
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-20 11:09:32 +0530

    Time: 1 ms (78.32%), Space: 10.2 MB (75.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool isSafe(int r,int c,int n,vector<string>&board)
+    bool isSafe(int r,int c,int n,vector<string>& board)
     {
         for(int i=0;i<n;i++)
         {
-            if(board[i][c]=='Q')
+            if(board[r][i]=='Q')
             {
                 return false;
             }
         }
-        for(int j=0;j<n;j++)
+        for(int i=0;i<n;i++)
         {
-            if(board[r][j]=='Q')
+            if(board[i][c]=='Q')
             {
                 return false;
             }
@@ -32,21 +32,20 @@ public:
         }
         return true;
     }
-    void helper(int r,int n,vector<string>&board,vector<vector<string>>&ans)
+    void helper(int r,int n,vector<string> &board,vector<vector<string>> &ans)
     {
         if(r==n)
         {
             ans.push_back({board});
             return;
         }
-
-        for(int j=0;j<n;j++)
+        for(int i=0;i<n;i++)
         {
-            if(isSafe(r,j,n,board))
+            if(isSafe(r,i,n,board))
             {
-                board[r][j]='Q';
+                board[r][i]='Q';
                 helper(r+1,n,board,ans);
-                board[r][j]='.';
+                board[r][i]='.';
             }
         }
     }
