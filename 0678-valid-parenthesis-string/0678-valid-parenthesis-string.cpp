4ba41c8a24a69177386e412c546792293ee9a944
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-23 11:23:42 +0530

    Time: 0 ms (100%), Space: 8 MB (93.51%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int low=0;
+        int high=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+            {
+                low++;
+                high++;
+            }
+            else if(s[i]==')')
+            {
+                low--;
+                high--;
+            }
+            else
+            {
+                low--;
+                high++;
+            }
+
+            if(low<0)
+            {
+                low=0;
+            }
+            if(high<0)
+            {
+                return false;
+            }
+        }
+        if(low==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
