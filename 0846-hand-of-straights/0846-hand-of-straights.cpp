b29adc6ed291d997eacb0486c7e1888fa51775e8
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-23 10:01:09 +0530

    Time: 41 ms (43.16%), Space: 32.3 MB (38.36%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if(n % groupSize != 0) return false;
-
-        map<int, int> freq;
-
-        for(int num : hand) {
-            freq[num]++;
+        int n=hand.size();
+        if(n%groupSize!=0)
+        {
+            return false;
         }
-
-        for(auto &it : freq) {
-            int start = it.first;
-            int count = it.second;
-
-            if(count > 0) {
-                // try to form group starting at 'start'
-                for(int i = 0; i < groupSize; i++) {
-                    int curr = start + i;
-
-                    if(freq[curr] < count) {
+        map<int,int>mp;
+        for(int num:hand)
+        {
+            mp[num]++;
+        }
+        for(auto it:mp)
+        {
+            int el=it.first;
+            int cnt=it.second;
+            if(cnt>0)
+            {
+                for(int i=0;i<groupSize;i++)
+                {
+                    if(mp[el+i]<cnt)
+                    {
                         return false;
                     }
-
-                    freq[curr] -= count;
+                    mp[el+i]-=cnt;
                 }
             }
         }
-
         return true;
     }
 };
