372df96d3e9e8452508aefce23ecee8870f929a3
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-04-25 11:31:27 +0530

    Time: 0 ms (100%), Space: 11.6 MB (62.35%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
+        int n=digits.size();
         for(int i=n-1;i>=0;i--)
         {
             if(digits[i]<9)
