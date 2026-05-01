e898da62bef1bbc01dc406e2b299c8a76f4ad852
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-01 11:22:05 +0530

    Time: 0 ms (100%), Space: 10.5 MB (79.17%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans(n+1,0);
         for(int i=1;i<=n;i++)
         {
-            ans[i] = ans[i>>1]+(i&1);
+            ans[i]=ans[i>>1]+(i&1);
         }
         return ans;
     }
