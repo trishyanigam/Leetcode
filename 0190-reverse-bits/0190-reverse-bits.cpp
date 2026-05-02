cee068f8cd86c98126c04decc9f521711e3e2896
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-02 11:24:57 +0530

    Time: 0 ms (100%), Space: 8.2 MB (44.84%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++)
+        {
+            int bit = n&1;
+            ans=ans<<1;
+            ans=ans|bit;
+            n=n>>1;
+        }
+        return ans;
+    }
+};
