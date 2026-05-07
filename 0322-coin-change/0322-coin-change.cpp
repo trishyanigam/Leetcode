f4047eb456aba9d34cc9ec9c80d36e91c379b178
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-07 11:51:52 +0530

    Time: 43 ms (37.41%), Space: 49.3 MB (26.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -18,15 +18,15 @@ public:
         int take = INT_MAX;
         if(coins[i]<=amount)
         {
-            take = 1+helper(i,coins,amount-coins[i],dp);
+            take=1+helper(i,coins,amount-coins[i],dp);
         }
-        dp[i][amount]=min(take,notTake);
+        dp[i][amount] = min(take,notTake);
         return dp[i][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=helper(n-1,coins,amount,dp);
+        int ans = helper(n-1,coins,amount,dp);
         if(ans>=1e9)
         {
             return -1;
