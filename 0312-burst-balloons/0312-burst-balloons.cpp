e61f3449fb238c198ef91ac7ea645756e10c9934
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-14 07:42:00 +0530

    Time: 207 ms (21.73%), Space: 13.7 MB (78.87%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<int>&nums,vector<vector<int>>&dp)
+    int helper(int i,int j,vector<int>& nums,vector<vector<int>>& dp)
     {
         if(i+1==j)
         {
@@ -13,17 +13,19 @@ public:
         int ans=0;
         for(int k=i+1;k<j;k++)
         {
-            int coins = nums[i]*nums[k]*nums[j]+helper(i,k,nums,dp)+helper(k,j,nums,dp);
-            ans=max(ans,coins);
+            int coins = nums[i]*nums[k]*nums[j];
+            coins+=helper(i,k,nums,dp);
+            coins+=helper(k,j,nums,dp);
+
+            ans = max(ans,coins);
         }
-        dp[i][j]=ans;
-        return dp[i][j];
+        return dp[i][j] = ans;
     }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,-1));
+        vector<vector<int>>dp(n+1,vector<int>(n+2,-1));
         return helper(0,n+1,nums,dp);
     }
 };
