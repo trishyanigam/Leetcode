a901f05ea6b6c4d312b8a3c355c5bf85e1278e8b
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-14 08:30:00 +0530

    Time: 3 ms (46.18%), Space: 9.7 MB (11.11%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool helper(int i,int j,string &s,string &p,int n,int m,vector<vector<int>>& dp)
+    bool helper(int i,int j,string& s, string& p,int n,int m,vector<vector<int>>& dp)
     {
         if(j==m)
         {
-            return i==n;
+            return (i==n);
         }
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        bool match = (i<s.size() && (s[i]==p[j] || p[j]=='.'));
+        bool match = (i<n && (s[i]==p[j] || p[j]=='.'));
         bool ans;
-        if(j+1<p.size() && p[j+1]=='*')
+        if(j+1<m && p[j+1]=='*')
         {
             ans = helper(i,j+2,s,p,n,m,dp) || (match && helper(i+1,j,s,p,n,m,dp));
         }
