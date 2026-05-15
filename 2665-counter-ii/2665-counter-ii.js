9cfca880cd5847382d5e528e61298da5ff39bd7c
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-15 09:52:09 +0530

    Time: 53 ms (39.07%), Space: 58.2 MB (5.9%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let curr = init;
+    return{
+        increment: function(){
+            curr++;
+            return curr;
+        },
+        decrement: function(){
+            curr--;
+            return curr;
+        },
+        reset: function(){
+            curr = init;
+            return curr;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
