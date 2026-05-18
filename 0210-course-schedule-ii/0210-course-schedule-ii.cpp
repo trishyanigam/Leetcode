01a4d493bc41515f8f11a3ba6150cfead63ffdbb
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-18 11:21:52 +0530

    Time: 7 ms (33.01%), Space: 19 MB (20.85%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
-    bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis1,vector<int>&vis2,stack<int>&st)
+    bool dfs(int node,vector<int>& vis,vector<int>& vis2,stack<int>& st,vector<int> adj[])
     {
-        vis1[node]=1;
-        vis2[node]=1;
-        for(int neigh:adj[node])
+        vis[node] = 1;
+        vis2[node] = 1;
+        for(auto it:adj[node])
         {
-            if(vis1[neigh]==0)
+            if(!vis[it])
             {
-                if(dfs(neigh,adj,vis1,vis2,st))
+                if(dfs(it,vis,vis2,st,adj))
                 {
                     return true;
                 }
             }
-            else if(vis2[neigh])
+            else if(vis2[it])
             {
                 return true;
             }
         }
-        vis2[node]=0;
         st.push(node);
+        vis2[node]=0;
         return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> ans;
-        vector<vector<int>>adj(numCourses);
-        for(auto &p:prerequisites)
+        vector<int> adj[numCourses];
+        for(auto it:prerequisites)
         {
-            int u=p[0];
-            int v=p[1];
+            int u = it[0];
+            int v = it[1];
             adj[v].push_back(u);
         }
-        vector<int>vis1(numCourses,0);
+        vector<int>vis(numCourses,0);
         vector<int>vis2(numCourses,0);
         stack<int>st;
         for(int i=0;i<numCourses;i++)
         {
-            if(vis1[i]==0)
+            if(!vis[i])
             {
-                if(dfs(i,adj,vis1,vis2,st))
+                if(dfs(i,vis,vis2,st,adj))
                 {
                     return {};
                 }
             }
         }
-
         while(!st.empty())
         {
-            int el=st.top();
+            int el = st.top();
             st.pop();
             ans.push_back(el);
         }
