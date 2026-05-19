dee1db3eb1d26bd27bcdd0732bc7aebfe2567776
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-19 11:26:57 +0530

    Time: 3 ms (87.93%), Space: 23.5 MB (46.95%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -3,15 +3,12 @@ public:
     void dfs(int r,int c,int n,int m,vector<vector<int>>& heights,vector<vector<int>>& vis)
     {
         vis[r][c] = 1;
-
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
-
         for(int i=0;i<4;i++)
         {
             int nr = r+dr[i];
             int nc = c+dc[i];
-
             if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && heights[nr][nc]>=heights[r][c])
             {
                 dfs(nr,nc,n,m,heights,vis);
@@ -21,34 +18,31 @@ public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        
-        vector<vector<int>>pacific(n,vector<int>(m,0));
-        vector<vector<int>>atlantic(n,vector<int>(m,0));
 
+        vector<vector<int>>p(n,vector<int>(m,0));
+        vector<vector<int>>a(n,vector<int>(m,0));
         for(int j=0;j<m;j++)
         {
-            dfs(0,j,n,m,heights,pacific);
+            dfs(0,j,n,m,heights,p);
         }
         for(int i=0;i<n;i++)
         {
-            dfs(i,0,n,m,heights,pacific);
+            dfs(i,0,n,m,heights,p);
         }
         for(int i=0;i<n;i++)
         {
-            dfs(i,m-1,n,m,heights,atlantic);
+            dfs(i,m-1,n,m,heights,a);
         }
         for(int j=0;j<m;j++)
         {
-            dfs(n-1,j,n,m,heights,atlantic);
+            dfs(n-1,j,n,m,heights,a);
         }
-
-        vector<vector<int>>ans;
-
+        vector<vector<int>> ans;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(pacific[i][j] && atlantic[i][j])
+                if(p[i][j] && a[i][j])
                 {
                     ans.push_back({i,j});
                 }
