ed05c4ce82ee5b917b13fe8aba7fbf15c3caa780
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-21 22:33:31 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,18 +6,24 @@ public:
         int k=m+n-1;
         while(i>=0 && j>=0)
         {
-            if(nums1[i]<nums2[j])
+            if(nums1[i]>nums2[j])
             {
-                nums1[k--]=nums2[j--];
+                nums1[k] = nums1[i];
+                i--;
+                k--;
             }
             else
             {
-                nums1[k--]=nums1[i--];
+                nums1[k] = nums2[j];
+                j--;
+                k--;
             }
         }
         while(j>=0)
         {
-            nums1[k--]=nums2[j--];
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
