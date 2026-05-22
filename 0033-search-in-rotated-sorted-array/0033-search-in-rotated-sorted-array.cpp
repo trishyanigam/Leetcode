95bd1b20bbd7a8504e9e19cf66f08185635eb367
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-22 09:29:44 +0530

    Time: 0 ms (100%), Space: 15.1 MB (95.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         int high=n-1;
         while(low<=high)
         {
-            int mid = (low+high)/2;
+            int mid = low+(high-low)/2;
             if(nums[mid]==target)
             {
                 return mid;
