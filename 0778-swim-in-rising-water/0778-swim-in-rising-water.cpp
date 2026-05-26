65ae5cb7248ba02a2b678c1b5a49062066fd3213
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-26 08:42:48 +0530

    Time: 27 ms (14.64%), Space: 15.8 MB (20.26%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -4,21 +4,24 @@ public:
         int n = grid.size();
         priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
         vector<vector<int>>vis(n,vector<int>(n,0));
-        pq.push({grid[0][0],0,0});
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
+        pq.push({grid[0][0],0,0});
         while(!pq.empty())
         {
             int time = pq.top()[0];
             int r = pq.top()[1];
             int c = pq.top()[2];
-
             pq.pop();
-
-            if(vis[r][c]) continue;
+            if(vis[r][c])
+            {
+                continue;
+            }
             vis[r][c] = 1;
             if(r==n-1 && c==n-1)
-            return time;
+            {
+                return time;
+            }
             for(int i=0;i<4;i++)
             {
                 int nr = r+dr[i];
