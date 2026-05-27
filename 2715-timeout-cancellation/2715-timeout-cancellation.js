0b8386388c910b1ad54ace66fed39180f379c3e4
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-05-27 08:08:36 +0530

    Time: 60 ms (33.71%), Space: 53.1 MB (94.73%) - LeetHub

--- 2715-timeout-cancellation/2715-timeout-cancellation.js
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -8,11 +8,11 @@ var cancellable = function(fn, args, t) {
     const id = setTimeout(()=>{
         fn(...args);
     },t);
-
+    
     return function cancelFn()
     {
         clearTimeout(id);
-    };
+    }
 };
 
 /**
