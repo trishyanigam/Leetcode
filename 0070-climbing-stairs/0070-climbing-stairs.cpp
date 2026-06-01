87e951f25d2d9922b5604d6cd94a1cb27cd305b1
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-01 10:31:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,13 +5,14 @@ public:
         {
             return n;
         }
-        vector<int>dp(n+1,-1);
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev2 = 1;
+        int prev1 = 2;
         for(int i=3;i<=n;i++)
         {
-            dp[i] = dp[i-1]+dp[i-2];
+            int curr = prev2+prev1;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
 };
