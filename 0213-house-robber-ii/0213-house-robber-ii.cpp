a7495c5d094547439aa9db476c2032583f6e3f0d
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-04 10:07:12 +0530

    Time: 0 ms (100%), Space: 10.8 MB (50.65%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int helper(int i,vector<int>& arr,vector<int>& dp)
+    int helper(int i,vector<int>& nums,vector<int>& dp)
     {
         if(i==0)
         {
-            return arr[0];
+            return nums[0];
         }
-        if(i==-1)
+        if(i<0)
         {
             return 0;
         }
@@ -14,10 +14,9 @@ public:
         {
             return dp[i];
         }
-        int take = arr[i]+helper(i-2,arr,dp);
-        int notTake = 0+helper(i-1,arr,dp);
-        dp[i] = max(take,notTake);
-        return dp[i];
+        int notTake = helper(i-1,nums,dp);
+        int take = nums[i]+helper(i-2,nums,dp);
+        return dp[i] = max(take,notTake);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
@@ -25,30 +24,26 @@ public:
         {
             return nums[0];
         }
-        vector<int> t1;
-        vector<int> t2;
-        for(int i=0;i<n;i++)
-        {
-            if(i!=n-1)
-            {
-                t1.push_back(nums[i]);
-            }
-        }
-
+        vector<int> nums1;
+        vector<int> nums2;
         for(int i=0;i<n;i++)
         {
             if(i!=0)
             {
-                t2.push_back(nums[i]);
+                nums1.push_back(nums[i]);
             }
         }
-
-        vector<int> dp1(n,-1);
-        vector<int> dp2(n,-1);
-
-        int ans1 = helper(n-2,t1,dp1);
-        int ans2 = helper(n-2,t2,dp2);
-
+        for(int i=0;i<n;i++)
+        {
+            if(i!=n-1)
+            {
+                nums2.push_back(nums[i]);
+            }
+        }
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        int ans1 = helper(n-2,nums1,dp1);
+        int ans2 = helper(n-2,nums2,dp2);
         return max(ans1,ans2);
     }
 };
