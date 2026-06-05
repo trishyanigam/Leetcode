c50a813da011d3fee84408a0c36b2a42fcf60733
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-05 11:03:12 +0530

    Time: 73 ms (47.21%), Space: 67.7 MB (24.62%) - LeetHub

--- 2705-compact-object/2705-compact-object.js
+++ 2705-compact-object/2705-compact-object.js
@@ -3,18 +3,22 @@
  * @return {Object|Array}
  */
 var compactObject = function(obj) {
-    if(obj===null || typeof obj !== "object")
+    if(obj===null || typeof obj!=="object")
     {
         return obj;
     }
+    
     if(Array.isArray(obj))
     {
         return obj.filter(Boolean).map(compactObject);
     }
+
     let res = {};
+
     for(const key in obj)
     {
         const val = compactObject(obj[key]);
+
         if(Boolean(val))
         {
             res[key] = val;
