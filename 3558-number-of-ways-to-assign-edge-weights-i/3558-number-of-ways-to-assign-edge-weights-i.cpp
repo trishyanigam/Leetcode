6a2d8f8291228e2e9ee8d86dce76eb19882c480d
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-11 09:23:34 +0530

    Time: 310 ms (44.3%), Space: 336.7 MB (47.65%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    long long modPow(long long a, long long b) {
+        long long res = 1;
+
+        while (b) {
+            if (b & 1)
+                res = (res * a) % MOD;
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return res;
+    }
+
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+
+        vector<vector<int>> adj(n + 1);
+
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        queue<pair<int,int>> q;
+        q.push({1, 0});
+
+        vector<int> vis(n + 1, 0);
+        vis[1] = 1;
+
+        int maxDepth = 0;
+
+        while (!q.empty()) {
+            auto [node, depth] = q.front();
+            q.pop();
+
+            maxDepth = max(maxDepth, depth);
+
+            for (int nei : adj[node]) {
+                if (!vis[nei]) {
+                    vis[nei] = 1;
+                    q.push({nei, depth + 1});
+                }
+            }
+        }
+
+        if (maxDepth == 0) return 0;
+
+        return modPow(2, maxDepth - 1);
+    }
+};
