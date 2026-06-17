df1f228f75a6b96acb7de843fd294d2b8641f030
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-18 00:04:20 +0530

    Time: 11 ms (40.33%), Space: 44.8 MB (77.5%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,10 +3,10 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int n = g.size();
-        int m = s.size();
         int l=0;
         int r=0;
+        int n = g.size();
+        int m = s.size();
         while(l<n && r<m)
         {
             if(g[l]<=s[r])
