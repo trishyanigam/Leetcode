7d16a2ca50ed871bf167709b998cc21669c0d16a
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-21 12:52:32 +0530

    Time: 0 ms (100%), Space: 9.4 MB (29.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int helper(int r,int c,int m,int n,vector<vector<int>>& dp)
     {
+        if(r<0 || r>=m || c<0 || c>=n)
+        {
+            return 0;
+        }
         if(r==0 && c==0)
         {
             return 1;
-        }
-        if(r<0 || c<0 || r>=m || c>=n)
-        {
-            return 0;
         }
         if(dp[r][c]!=-1)
         {
