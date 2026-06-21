e051aa443d6a357c774501e15d0b64ffd1b3a97f
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-21 12:42:40 +0530

    Time: 42 ms (75.64%), Space: 20.9 MB (73.77%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    int helper(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
+    int helper(int i,int j1,int j2,int m,int n,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
     {
-        int m = grid.size();
-        int n = grid[0].size();
         if(j1<0 || j1>=n || j2<0 || j2>=n)
         {
             return -1e8;
@@ -20,7 +18,7 @@ public:
         {
             return dp[i][j1][j2];
         }
-        int cherry = 0;
+        int cherry =0;
         if(j1==j2)
         {
             cherry = grid[i][j1];
@@ -29,12 +27,12 @@ public:
         {
             cherry = grid[i][j1]+grid[i][j2];
         }
-        int maxi = 0;
+        int maxi=0;
         for(int dj1=-1;dj1<=1;dj1++)
         {
             for(int dj2=-1;dj2<=1;dj2++)
             {
-                maxi = max(maxi,helper(i+1,j1+dj1,j2+dj2,grid,dp));
+                maxi = max(maxi,helper(i+1,j1+dj1,j2+dj2,m,n,grid,dp));
             }
         }
         return dp[i][j1][j2] = cherry+maxi;
@@ -43,6 +41,6 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        return helper(0,0,n-1,grid,dp);
+        return helper(0,0,n-1,m,n,grid,dp);
     }
 };
