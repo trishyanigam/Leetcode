0bce5a009d887866645f3a0a9c94ec6b80f2232f
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-06-23 10:10:44 +0530

    Time: 47 ms (59.93%), Space: 54.8 MB (8.57%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -6,7 +6,7 @@ public:
         {
             return true;
         }
-        if(i>=0 && j<0)
+        if(j<0 && i>=0)
         {
             return false;
         }
@@ -31,14 +31,14 @@ public:
         }
         if(p[j]=='*')
         {
-            return dp[i][j] = helper(i-1,j,s,p,dp) || helper(i,j-1,s,p,dp);
+            return dp[i][j] =helper(i-1,j,s,p,dp) || helper(i,j-1,s,p,dp);
         }
         return dp[i][j] = false;
     }
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return helper(n-1,m-1,s,p,dp);
+        int m = s.size();
+        int n = p.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return helper(m-1,n-1,s,p,dp);
     }
 };
