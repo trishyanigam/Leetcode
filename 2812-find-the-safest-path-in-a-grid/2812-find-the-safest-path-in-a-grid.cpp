62a6e4e32fb88afb10388f13603ea781e9661b6a
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-07-01 17:47:20 +0530

    Time: 552 ms (33.01%), Space: 178.8 MB (46.3%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        queue<pair<int,int>> q;
+
+        // Multi-source BFS
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while(!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < n &&
+                   dist[nr][nc] == INT_MAX) {
+
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        priority_queue<vector<int>> pq;
+        vector<vector<int>> best(n, vector<int>(n, -1));
+
+        pq.push({dist[0][0], 0, 0});
+        best[0][0] = dist[0][0];
+
+        while(!pq.empty()) {
+
+            auto curr = pq.top();
+            pq.pop();
+
+            int safe = curr[0];
+            int r = curr[1];
+            int c = curr[2];
+
+            if(r == n - 1 && c == n - 1)
+                return safe;
+
+            if(safe < best[r][c])
+                continue;
+
+            for(int k = 0; k < 4; k++) {
+
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < n) {
+
+                    int newSafe = min(safe, dist[nr][nc]);
+
+                    if(newSafe > best[nr][nc]) {
+
+                        best[nr][nc] = newSafe;
+                        pq.push({newSafe, nr, nc});
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+};
