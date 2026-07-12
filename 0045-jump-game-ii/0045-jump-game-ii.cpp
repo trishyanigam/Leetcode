e6393907e5e6558802b71589a96ef369c353607e
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-07-12 11:41:25 +0530

    Time: 0 ms (100%), Space: 20.6 MB (62.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int l = 0;
-        int r = 0;
+        int l=0,r=0;
         int ans = 0;
         while(r<n-1)
         {
