192d4b314109c6953a6896da5f74665587434777
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-07-16 18:58:21 +0530

    Time: 8 ms (38.32%), Space: 10.8 MB (71.16%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,8 +3,8 @@ public:
     int characterReplacement(string s, int k) {
         int l = 0;
         int ans = 0;
+        vector<int> freq(26,0);
         int maxFreq = 0;
-        vector<int>freq(26,0);
         for(int r=0;r<s.size();r++)
         {
             freq[s[r]-'A']++;
@@ -14,7 +14,7 @@ public:
                 freq[s[l]-'A']--;
                 l++;
             }
-            ans=max(ans,r-l+1);
+            ans = max(ans,r-l+1);
         }
         return ans;
     }
