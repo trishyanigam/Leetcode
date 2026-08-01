781d5a165b02ad9e1bb6edf57e8e8ce1b8c019ef
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-01 09:19:46 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp)
+    {
+        if(i == j)
+            return nums[i];
+
+        if(dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int left = nums[i] - solve(i + 1, j, nums, dp);
+        int right = nums[j] - solve(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(left, right);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
+
+        return solve(0, n - 1, nums, dp) >= 0;
+    }
+};
