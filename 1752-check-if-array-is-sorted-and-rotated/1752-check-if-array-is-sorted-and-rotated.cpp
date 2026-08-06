7123508ad82997ef622539f2264513fbccb7fab8
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-06 18:04:36 +0530

    Time: 0 ms (100%), Space: 11.3 MB (56.33%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,7 +5,7 @@ public:
         int cnt = 0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]>nums[(i+1)%n])
+            if(nums[(i+1)%n]<nums[i])
             {
                 cnt++;
             }
