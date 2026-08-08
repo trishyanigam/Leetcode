3bfa10947ba5c934a4beb4cc903b19de50577a76
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-08 10:56:33 +0530

    Time: 0 ms (100%), Space: 9.5 MB (89.9%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -7,7 +7,7 @@ public:
             vector<int>row(i+1,1);
             for(int j=1;j<i;j++)
             {
-                row[j] = ans[i-1][j-1]+ans[i-1][j];
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
             }
             ans.push_back(row);
         }
