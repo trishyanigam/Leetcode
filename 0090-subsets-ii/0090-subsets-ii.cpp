856b915492b702391af870eee3e94711f8d0d2fd
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-16 10:58:26 +0530

    Time: 0 ms (100%), Space: 10.3 MB (85.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void helper(int st,vector<int>& nums,vector<int>& path,vector<vector<int>>& ans)
+    void helper(int st,int n,vector<int>& nums,vector<int>& path,vector<vector<int>>& ans)
     {
         ans.push_back(path);
-        for(int i=st;i<nums.size();i++)
+        
+        for(int i=st;i<n;i++)
         {
             if(i>st && nums[i]==nums[i-1])
             {
                 continue;
             }
             path.push_back(nums[i]);
-            helper(i+1,nums,path,ans);
+            helper(i+1,n,nums,path,ans);
             path.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       vector<vector<int>> ans;
-       vector<int> path;
-       helper(0,nums,path,ans); 
-       return ans;
+        vector<vector<int>> ans;
+        vector<int> path;
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        helper(0,n,nums,path,ans);
+        return ans;
     }
 };
