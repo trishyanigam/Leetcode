e10a6856688d4453f4e34e362bec26d215741708
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-16 11:31:05 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void helper(int st,vector<string> mapping,string &path,string digits,vector<string> &ans)
+    void helper(int st,string& digits,vector<string>& mapping,string &path,vector<string>& ans)
     {
         if(st==digits.size())
         {
             ans.push_back(path);
             return;
         }
-
         string letter = mapping[digits[st]-'0'];
         for(char ch:letter)
         {
             path.push_back(ch);
-            helper(st+1,mapping,path,digits,ans);
+            helper(st+1,digits,mapping,path,ans);
             path.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
         string path = "";
-        vector<string> mapping = {
-            "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
-        };
-        helper(0,mapping,path,digits,ans);
-        return ans;
+        vector<string> mapping = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        helper(0,digits,mapping,path,ans);
+        return ans; 
     }
 };
