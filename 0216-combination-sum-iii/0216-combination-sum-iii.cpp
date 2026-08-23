6550e1b0f12e3e6c360f38db0f9d9ca3924d01e2
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-23 17:01:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (83.16%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    void helper(int i,int s,int k,int n,vector<int>& arr,vector<int>& path,vector<vector<int>>& ans)
+    void helper(int i,int n,int k,vector<int>& nums,vector<int>& path,vector<vector<int>>& ans)
     {
-        if(i==s)
+        if(k==0 && n==0)
         {
-            if(n==0 && k==0)
-            {
-                ans.push_back(path);
-            }
+            ans.push_back(path);
             return;
         }
-        if(arr[i]<=n)
+        if(i==9)
         {
-            path.push_back(arr[i]);
-            helper(i+1,s,k-1,n-arr[i],arr,path,ans);
+            return;
+        }
+        if(nums[i]<=n)
+        {
+            path.push_back(nums[i]);
+            helper(i+1,n-nums[i],k-1,nums,path,ans);
             path.pop_back();
         }
-        helper(i+1,s,k,n,arr,path,ans);
+        helper(i+1,n,k,nums,path,ans);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> arr = {1,2,3,4,5,6,7,8,9};
-        vector<vector<int>> ans;
+        vector<int>nums = {1,2,3,4,5,6,7,8,9};
         vector<int> path;
-        helper(0,9,k,n,arr,path,ans);
+        vector<vector<int>> ans;
+        helper(0,n,k,nums,path,ans);
         return ans;
     }
 };
