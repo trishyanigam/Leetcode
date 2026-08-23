5c0675659c0f308b354764ec03003594a3b50b50
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-23 16:19:44 +0530

    Time: 3 ms (51.91%), Space: 14 MB (77.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    void helper(int i,int sum,vector<int>& candidates, int target,vector<int>& path,vector<vector<int>>& ans)
+    void helper(int i,int n,vector<int>& arr,int tar,vector<int>& path,vector<vector<int>>& ans)
     {
-        if(i==candidates.size())
+        if(tar==0)
         {
-            if(sum==target)
-            {
-                ans.push_back(path);
-            }
+            ans.push_back(path);
             return;
         }
-        if(sum>target)
+        if(i==n)
         {
             return;
         }
-        path.push_back(candidates[i]);
-        helper(i,sum+candidates[i],candidates,target,path,ans);
-        path.pop_back();
+        if(arr[i]<=tar)
+        {
+            path.push_back(arr[i]);
+            helper(i,n,arr,tar-arr[i],path,ans);
+            path.pop_back();
+        }
 
-        helper(i+1,sum,candidates,target,path,ans);
+        helper(i+1,n,arr,tar,path,ans);
+        
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>path;
-        helper(0,0,candidates,target,path,ans);
+        int n = candidates.size();
+        vector<int> path;
+        vector<vector<int>> ans;
+        helper(0,n,candidates,target,path,ans);
         return ans;
     }
 };
