65d6b89de0b50d7b0f68d99d11f8fe550afcb5c2
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-24 14:47:55 +0530

    Time: 25 ms (71.87%), Space: 17.3 MB (51.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    void dfs(int i,int j,int n,int m,vector<vector<char>>& grid,vector<vector<bool>>& vis)
+    void dfs(int i,int j,int n,int m,vector<vector<char>>& grid,vector<vector<int>>& vis)
     {
-        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0' || vis[i][j])
+        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || grid[i][j]=='0')
         {
             return;
         }
-
-        vis[i][j] = true;
-        
+        vis[i][j] = 1;
         dfs(i-1,j,n,m,grid,vis);
         dfs(i+1,j,n,m,grid,vis);
         dfs(i,j-1,n,m,grid,vis);
@@ -17,9 +15,8 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        vector<vector<int>>vis(n,vector<int>(m,0));
         int ans = 0;
-
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
