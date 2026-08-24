2fef3dde55fe76e0b886b406bac3da4f8dbf742e
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-24 16:44:43 +0530

    Time: 3 ms (75.82%), Space: 20.4 MB (12.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool dfs(int i,vector<vector<int>>& adj,vector<int>& vis,vector<int>& vis2)
+    bool dfs(int node,vector<vector<int>>& adj,vector<int>& vis,vector<int>& vis2)
     {
-        vis[i]=1;
-        vis2[i]=1;
-        for(auto it:adj[i])
+        vis[node]=1;
+        vis2[node]=1;
+        for(auto it:adj[node])
         {
             if(!vis[it])
             {
@@ -18,15 +18,15 @@ public:
                 return true;
             }
         }
-        vis2[i]=0;
+        vis2[node] = 0;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
         for(auto it:prerequisites)
         {
-            int u=it[0];
-            int v=it[1];
+            int u = it[0];
+            int v = it[1];
             adj[v].push_back(u);
         }
         vector<int>vis(numCourses,0);
