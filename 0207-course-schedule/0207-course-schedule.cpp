af635f2473096440a3ce90d989759329e618359c
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-24 16:00:39 +0530

    Time: 6 ms (50.61%), Space: 20.4 MB (12.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>& vis,vector<int>& vis2,vector<int> adj[])
+    bool dfs(int i,vector<vector<int>>& adj,vector<int>& vis,vector<int>& vis2)
     {
-        vis[node] = 1;
-        vis2[node] = 1;
-        for(auto it:adj[node])
+        vis[i]=1;
+        vis2[i]=1;
+        for(auto it:adj[i])
         {
             if(!vis[it])
             {
-                if(dfs(it,vis,vis2,adj))
+                if(dfs(it,adj,vis,vis2))
                 {
                     return true;
                 }
@@ -18,26 +18,24 @@ public:
                 return true;
             }
         }
-        vis2[node] = 0;
+        vis2[i]=0;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>adj[numCourses];
+        vector<vector<int>>adj(numCourses);
         for(auto it:prerequisites)
         {
-            int u = it[0];
-            int v = it[1];
+            int u=it[0];
+            int v=it[1];
             adj[v].push_back(u);
         }
-
-        vector<int> vis(numCourses,0);
-        vector<int> vis2(numCourses,0);
-
+        vector<int>vis(numCourses,0);
+        vector<int>vis2(numCourses,0);
         for(int i=0;i<numCourses;i++)
         {
             if(!vis[i])
             {
-                if(dfs(i,vis,vis2,adj))
+                if(dfs(i,adj,vis,vis2))
                 {
                     return false;
                 }
