dfd52554ad9beac3a9a92c01de96641691fcc6af
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-27 16:28:10 +0530

    Time: 37 ms (83.63%), Space: 21.1 MB (14.38%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int helper(int i,int j1,int j2,int m,int n,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
+    int helper(int i,int j1,int j2,int n,int m,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp)
     {
-        if(j1<0 || j1>=n || j2<0 || j2>=n)
+        if(j1<0 || j1>=m || j2<0 || j2>=m)
         {
             return -1e8;
         }
-        if(i==m-1)
+        if(i==n-1)
         {
             if(j1==j2)
             {
                 return grid[i][j1];
             }
-            return grid[i][j1]+grid[i][j2];
+            else
+            {
+                return grid[i][j1]+grid[i][j2];
+            }
         }
         if(dp[i][j1][j2]!=-1)
         {
             return dp[i][j1][j2];
         }
-        int cherry =0;
+        int cherry = 0;
         if(j1==j2)
         {
             cherry = grid[i][j1];
@@ -27,20 +30,20 @@ public:
         {
             cherry = grid[i][j1]+grid[i][j2];
         }
-        int maxi=0;
+        int maxi = INT_MIN;
         for(int dj1=-1;dj1<=1;dj1++)
         {
             for(int dj2=-1;dj2<=1;dj2++)
             {
-                maxi = max(maxi,helper(i+1,j1+dj1,j2+dj2,m,n,grid,dp));
+                maxi = max(maxi,helper(i+1,j1+dj1,j2+dj2,n,m,grid,dp));
             }
         }
         return dp[i][j1][j2] = cherry+maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
-        return helper(0,0,n-1,m,n,grid,dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(m,vector<int>(m,-1)));
+        return helper(0,0,m-1,n,m,grid,dp);
     }
 };
