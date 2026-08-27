238876ed094883411c8b7fdc4b94f94842d746be
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-27 09:53:02 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.5%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int helper(int r,int c,int m,int n,vector<vector<int>> &grid,vector<vector<int>>& dp)
+    int helper(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp)
     {
-        if(r<0 || r>=m || c<0 || c>=n || grid[r][c]==1)
+        if(i<0 || j<0 || obstacleGrid[i][j]==1)
         {
             return 0;
         }
-        if(r==0 && c==0)
+        if(i==0 && j==0)
         {
             return 1;
         }
-        if(dp[r][c]!=-1)
+        if(dp[i][j]!=-1)
         {
-            return dp[r][c];
+            return dp[i][j];
         }
-        int up = helper(r-1,c,m,n,grid,dp);
-        int left = helper(r,c-1,m,n,grid,dp);
-        return dp[r][c] = up+left;
+        int up = helper(i-1,j,obstacleGrid,dp);
+        int left = helper(i,j-1,obstacleGrid,dp);
+        return dp[i][j] = up+left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return helper(m-1,n-1,m,n,obstacleGrid,dp);
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return helper(n-1,m-1,obstacleGrid,dp);
     }
 };
