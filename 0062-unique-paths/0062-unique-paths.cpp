1ad0ea17965b57ba13cb6eb8caf50cf0ef743af3
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-28 13:09:38 +0530

    Time: 2 ms (20.94%), Space: 9.4 MB (30.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int helper(int i,int j,vector<vector<int>>&dp)
+    int helper(int i,int j,vector<vector<int>>& dp)
     {
-        if(i==0 && j==0)
-        {
-            return 1;
-        }
         if(i<0 || j<0)
         {
             return 0;
+        }
+        if(i==0 && j==0)
+        {
+            return 1;
         }
         if(dp[i][j]!=-1)
         {
