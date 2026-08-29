be80a211b0d536ea2984d1a3c9765410dcb35e5f
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-08-29 13:18:51 +0530

    Time: 38 ms (49.91%), Space: 49.3 MB (30.89%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int helper(int i,vector<int>& coins, int amount,vector<vector<int>>& dp)
+    int helper(int i,vector<int>& coins,int amt,vector<vector<int>>& dp)
     {
         if(i==0)
         {
-            if(amount%coins[0]==0)
+            if(amt%coins[0]==0)
             {
-                return amount/coins[0];
+                return amt/coins[0];
             }
             return 1e9;
         }
-        if(dp[i][amount]!=-1)
+        if(dp[i][amt]!=-1)
         {
-            return dp[i][amount];
+            return dp[i][amt];
         }
-        int notTake = helper(i-1,coins,amount,dp);
+        int notTake = helper(i-1,coins,amt,dp);
         int take = INT_MAX;
-        if(coins[i]<=amount)
+        if(coins[i]<=amt)
         {
-            take = 1+helper(i,coins,amount-coins[i],dp);
+            take = 1+helper(i,coins,amt-coins[i],dp);
         }
-        dp[i][amount] = min(take,notTake);
-        return dp[i][amount];
+        return dp[i][amt] = min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = helper(n-1,coins,amount,dp);
+        int ans =  helper(n-1,coins,amount,dp);
         if(ans>=1e9)
         {
             return -1;
