3a0d671527723e03a319746789cfb62ab425545a
trishyanigam <149099953+trishyanigam@users.noreply.github.com>
2026-09-08 11:29:42 +0530

    Time: 0 ms (100%), Space: 16.3 MB (36.07%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(int i,int buy,vector<int>& prices,vector<vector<int>>& dp,int n)
+    int helper(int i,int buy,int n,vector<int>& prices,vector<vector<int>>& dp)
     {
         if(i>=n)
         {
@@ -13,18 +13,17 @@ public:
         int profit = 0;
         if(buy)
         {
-            profit = max(-prices[i]+helper(i+1,0,prices,dp,n),0+helper(i+1,1,prices,dp,n));
+            profit = max(-prices[i]+helper(i+1,0,n,prices,dp),helper(i+1,1,n,prices,dp));
         }
         else
         {
-            profit = max(prices[i]+helper(i+2,1,prices,dp,n),0+helper(i+1,0,prices,dp,n));
+            profit = max(prices[i]+helper(i+2,1,n,prices,dp),helper(i+1,0,n,prices,dp));
         }
-        dp[i][buy] = profit;
-        return dp[i][buy];
+        return dp[i][buy] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
-        return helper(0,1,prices,dp,n);
+        return helper(0,1,n,prices,dp);
     }
 };
